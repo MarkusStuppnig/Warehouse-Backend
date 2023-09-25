24d0ac29e1efe987f287cf34b1cbdef46faaf4b9
Stuppnig Markus <markus@stuppnig.net>
2023-09-25 19:45:55 +0200

    Fix: CORS error for localhost:3000

--- src/main/java/at/ac/tgm/warehouse/warehouse/WarehouseController.java
+++ src/main/java/at/ac/tgm/warehouse/warehouse/WarehouseController.java
@@ -1,6 +1,7 @@
 package at.ac.tgm.warehouse.warehouse;
 
 import at.ac.tgm.warehouse.model.WarehouseData;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,26 +14,30 @@ public class WarehouseController {
 
     @Autowired
     private WarehouseService service;
-	
+
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/")
     public String warehouseMain() {
-    	String mainPage = "This is the warehouse application! (DEZSYS_WAREHOUSE_REST) <br/><br/>" +
+    	String mainPage = "Warehouse Application<br/><br/>" +
                           "<a href='http://localhost:8080/warehouse/001/data'>Link to warehouse/001/data</a><br/>" +
                           "<a href='http://localhost:8080/warehouse/001/xml'>Link to warehouse/001/xml</a><br/>" +
                           "<a href='http://localhost:8080/warehouse/001/transfer'>Link to warehouse/001/transfer</a><br/>";
         return mainPage;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value="/warehouse/{inID}/data", produces = MediaType.APPLICATION_JSON_VALUE)
     public WarehouseData warehouseData(@PathVariable String inID ) {
         return service.getWarehouseData( inID );
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value="/warehouse/{inID}/xml", produces = MediaType.APPLICATION_XML_VALUE)
     public WarehouseData warehouseDataXML( @PathVariable String inID ) {
         return service.getWarehouseData( inID );
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/warehouse/{inID}/transfer")
     public String warehouseTransfer( @PathVariable String inID ) {
         return service.getGreetings("Warehouse.Transfer!");
